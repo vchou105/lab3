d21b4eaf3fe090002eb0c8d7994b6077af027272
Vicky <v2chou@ucsd.edu>
2021-01-26 06:01:55 +0800

    changed project 1 & added css & about

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,30 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(this).text("Please wait...!");
+		//$(".jumbotron p").addClass("active");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
-	// example: $("#div-id").click(functionToCall);
+	$("#project1").click(projectClick);
 }
+
+function projectClick(e) {
+    // prevent the page from reloading
+	e.preventDefault();
+	// closest() returns the first ancestor of selected element
+	var containingProject = $(this).closest(".project");
+	// find() finds all descendant elements of selected element
+	var description = $(containingProject).find(".project-description");
+	$("#project1 img").toggle();
+	$(".project-description p").addClass("active");
+
+	if (description.length == 0) {
+		$(containingProject).append("<div class='project-description'><p>Collaborated with Latinas Contra Cancer to design an itinerary <br>	and website for their virtual walkathon, an annual event to foster more <br>support and awareness in our community.<br><br>A project in Design for America at UCSD where human-centered <br> design is used for social impact.</p></div>");
+	} else {
+	//	description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+		$(description).fadeToggle();
+	}
+}
+
